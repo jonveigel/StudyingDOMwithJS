9c681f893c4565ba1501ae2bb6d4ffbbc0b0ca11
Jon Veigel <cloudeca1@gmail.com>
2022-12-12 20:50:23 -0300

    GetElementByTagName Study JS

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ var myTitle2 = document.getElementById("title2");
 var myList = document.getElementById("myList");
 
 console.log(myTitle1.innerText);
-console.log(myTitle2.innerHTML);
+console.log(myTitle2.innerText);
 console.log(myList.innerText);
+
+
+var myTitleex = document.getElementsByTagName("h1");
+console.log(myTitleex[0].innerText);
