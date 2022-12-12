dd86a8251b5491e5a6629803ffa1bacff38ca3a2
Jon Veigel <cloudeca1@gmail.com>
2022-12-12 20:38:39 -0300

    Study and practice DOM with JS

---
+++ script.js
@@ -0,0 +1,9 @@
+var myTitle1 = document.getElementById("title1");
+
+var myTitle2 = document.getElementById("title2");
+
+var myList = document.getElementById("myList");
+
+console.log(myTitle1.innerText);
+console.log(myTitle2.innerHTML);
+console.log(myList.innerText);
